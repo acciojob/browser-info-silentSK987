0763dfedb912493f2ab495fbe39bacf942b7e499
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2024-03-06 16:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const browserInfo = document.querySelector("#browser-info");
+addEventListener("DOMContentLoaded",()=>{
+browserInfo.innerHTML = `You are using ${navigator.appName} version ${navigator.appVersion}`	
+})
